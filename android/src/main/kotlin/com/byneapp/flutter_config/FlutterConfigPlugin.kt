743b95fda42e9ff7a0cb5e473bd1440d98c5c860
anugrahsputra <anugrahsputra@gmail.com>
2025-08-18 17:37:13 +0700

    Refactor: Remove unused `registerWith` method
    This commit removes the unused `registerWith` method and its associated import (`io.flutter.plugin.common.PluginRegistry.Registrar`) from `FlutterConfigPlugin.kt`.

--- android/src/main/kotlin/com/byneapp/flutter_config/FlutterConfigPlugin.kt
+++ android/src/main/kotlin/com/byneapp/flutter_config/FlutterConfigPlugin.kt
@@ -10,7 +10,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
+//import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.lang.IllegalArgumentException
 import java.lang.reflect.Field
 
@@ -31,6 +31,7 @@ class FlutterConfigPlugin(private val context: Context? = null): FlutterPlugin, 
     applicationContext = null
   }
 
+  /**
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
@@ -38,6 +39,7 @@ class FlutterConfigPlugin(private val context: Context? = null): FlutterPlugin, 
       channel.setMethodCallHandler(FlutterConfigPlugin(registrar.activity()))
     }
   }
+  */
 
   override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "loadEnvVariables") {
